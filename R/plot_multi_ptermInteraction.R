d93c3c4d6de6c951da4f4a051fe826141541f1a6
danielward27 <danielward27@outlook.com>
2025-05-13 17:35:35 +0100

    Delete more (we might need multi, but deleted for now)

--- R/plot_multi_ptermInteraction.R
+++
@@ -1,8 +0,0 @@
-#' @rdname plot.ptermInteraction
-#' @export plot.ptermInteraction
-#' @export
-#'
-plot.multi.ptermInteraction <- function(x, ...) {
-  message("mgcViz does not know how to plot interactions. Returning NULL.")
-  return(invisible(NULL))
-}
--- R/plot_multi_ptermLogical.R
+++
@@ -1,8 +0,0 @@
-#' @rdname plot.ptermFactor
-#' @export plot.multi.ptermLogical
-#' @export
-#'
-plot.multi.ptermLogical <- function(x, ...) {
-  x[[1]]$name <- paste0(x[[1]]$name, "TRUE")
-  plot.multi.ptermNumeric(x, ...)
-}
